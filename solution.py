54559d2200dfa2ad77958f1cec26ced033123b9a
marryivanova <130369340+marryivanova@users.noreply.github.com>
2023-05-30 17:04:40 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,20 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    
+    # Найдём точечную оценку для среднего значения выборки
+    x_mean = x.mean()
+    
+    # Найдём стандартную ошибку среднего
+    stderr = stats.sem(x)
+    
+    # Вычислим квантиль t-распределения
+    t = abs(stats.t.ppf(0.025, len(x) - 1))
+    
+    # Найдём границы доверительного интервала
+    lower_bound = x_mean - t * stderr
+    upper_bound = x_mean + t * stderr
+    
+    # Выведем на экран доверительный интервал и вернём точечную оценку
+    print("Confidence interval: [{:.2f}, {:.2f}]".format(lower_bound, upper_bound))
+    return x_mean
